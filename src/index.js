35c5e9ed79aba5a21d676926beb2b8d188a077fa
julydeath <manojkarajada.mk@gmail.com>
2023-04-12 21:08:12 +0530

    init : ott-client added

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { AuthContextProvider } from "./authContext/AuthContext";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8800/api";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
